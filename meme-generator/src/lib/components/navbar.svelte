7f556ecc4a6fa20784fcf707ab75665d6bd39d4e
Champini <148045806+xchampini@users.noreply.github.com>
2024-12-11 21:51:17 +1100

    adds navbar, styling and some dummy routes for now (#10)
    imports navbar component into layout
    adds model for navbar items

---
+++ meme-generator/src/lib/components/navbar.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import type { NavbarItem } from '../models/navbar-item'
+    import { page } from '$app/stores'
+
+    export let navItems: NavbarItem[] = [
+        {displayName: 'Home', path: '/'},
+        {displayName: 'Contact', path: '/contact'},
+        {displayName: 'Templates', path: '/templates'}
+    ]
+
+    $:currentPath  = $page.url.pathname
+</script>
+
+<nav class="navbar">
+    <ul>
+        {#each navItems as {displayName, path}}
+        <li >
+            <a href={path} class="navbar-item {currentPath === path ? 'active-path' : ''}">
+                {displayName}
+            </a>
+        </li>
+        {/each}
+    </ul>
+</nav>
+
+<style>
+    .navbar {
+    height: 40px;
+    align-content: center;
+    padding-bottom: 5px;
+    padding-left: 15px;
+    background-color: #6320ee;
+    }
+
+    .navbar ul {
+        display: flex;
+        gap: 1rem;
+    }
+
+    .navbar-item  {
+            transition: color 0.2s ease;
+            color: #f8f0fb;
+
+            &:hover, &.active-path {
+                color: #cad5ca;
+            }
+
+            &:active {
+                color:#8075ff;
+            }
+    }
+
+</style>
--- meme-generator/src/routes/+layout.svelte
+++ meme-generator/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/components/navbar.svelte';
 	let { children } = $props();
 </script>
-
+<Navbar/> 
 {@render children()}
